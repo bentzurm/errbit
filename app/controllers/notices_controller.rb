b759344d23a9996eca010e3347fd756e0a6ac3b1
Shuky Dvir <shuky.dvir@biodata.com>
2012-02-13 21:58:51 +0200

    creating an issue in the issue tracker automaticaly for an error

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -6,6 +6,10 @@ class NoticesController < ApplicationController
   def create
     # params[:data] if the notice came from a GET request, raw_post if it came via POST
     @notice = App.report_error!(params[:data] || request.raw_post)
+    err = Err.find(@notice.err_id)
+    if err.problem.app.issue_tracker and err.problem.issue_link.nil?
+      err.problem.app.issue_tracker.create_issue err.problem
+    end
     respond_with @notice
   end
 
